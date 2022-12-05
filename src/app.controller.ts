8fe4d278dbef66e8fc041528d1aecde82d8e1f92
PedroLeale <pedroleale@gmail.com>
2022-12-05 15:30:32 -0300

    validate_jsons

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { StoreIpfsDto } from './dto/store-ipfs.dto';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post()  
+  storeIpfs(
+    @Body() storeIpfsDto: StoreIpfsDto,
+  ) {
+    return this.appService.storeIpfs(storeIpfsDto)
+
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { StoreIpfsDto } from './dto/store-ipfs.dto';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+
+
+
+  storeIpfs(storeIpfsDto: StoreIpfsDto) {
+    console.log(storeIpfsDto)
+
+
+
   }
 }
